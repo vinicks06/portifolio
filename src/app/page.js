ab97528bf9c7ac5fa19ac762cf80cd4bf0a86ee3
vinicks <viniciuscoelho0608@gmail.com>
2025-02-19 23:48:17 -0300

    primeiro commit

---
+++ src/app/page.js
@@ -0,0 +1,37 @@
+import ParticlesBackground from "./components/ParticlesBackground";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ParticlesBackground />
+
+      {/* Navbar */}
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>^</div>
+        <div className={styles.menuIcon}>☰</div>
+      </nav>
+
+      {/* Conteúdo Principal */}
+      <main className={styles.content}>
+        <h1 className={styles.title}>Olá, Eu sou o Vinícius</h1>
+          <div className={styles.flex}>
+          <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Aliquam elit diam, pellentesque sed felis at, tincidunt dignissim justo. Fusce nibh ligula, 
+          laoreet ut volutpat et.
+          </p>
+          <p className={styles.imagem}>Imagem</p>
+          </div>
+          <p className={styles.textBottom}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Praesent sagittis justo sed sapien blandit luctus. 
+          Phasellus lacinia tellus eget commodo facilisis. 
+          Cras facilisis est finibus justo consequat pellentesque. 
+          Integer rhoncus, nibh vel gravida dictum, orci tortor lacinia lacus, 
+          vitae scelerisque nisi odio at diam.
+          </p>
+      </main>
+    </div>
+  );
+}
