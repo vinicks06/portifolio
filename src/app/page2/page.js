f85eb3df78d6be0a08ab7eecfb67f2fa179fe721
vinicks <viniciuscoelho0608@gmail.com>
2025-03-09 18:47:16 -0300

    3.0

--- src/app/page2/page.js
+++ src/app/page2/page.js
@@ -1,17 +1,15 @@
-'use client';
-
-import ParticlesBackground from "./components/ParticlesBackground";
-import styles from "./Home.module.css";
-import { Autoplay } from "./components/Carrosel";
+import ParticlesBackground from "../components/ParticlesBackground";
+import styles from "../page2.module.css";
 import Image from 'next/image';
-import imgGit from './img/GitHub.png';
-import imgLinkedin from './img/Linkedin.png';
-import imgLogo from './img/Logo.png';
-import { useRouter } from 'next/navigation';
+import imgLogo from '../img/Logo.png';
+import imgJogo from '../img/jogo.png';
+import imgGerenciador from '../img/gerenciadorDeLivros.png';
+import imgPortfolio from '../img/Portfolio.png'
+import imgGit from '../img/GitHub.png';
+import imgLinkedin from '../img/Linkedin.png';
 
-export default function Home() {
-  const router = useRouter();
 
+export default function Page2() {
   return (
     <div className={styles.container}>
       <ParticlesBackground />
@@ -23,33 +21,42 @@ export default function Home() {
 
       {/* Conteúdo Principal */}
       <main className={styles.content}>
-        <h1 className={styles.title}>Olá, Eu sou o Vinícius</h1>
-          <div className={styles.flex}>
-            <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Aliquam elit diam, pellentesque sed felis at, tincidunt dignissim justo. Fusce nibh ligula, 
-            laoreet ut volutpat et.
-            </p>
-            <p className={styles.imagem}>Imagem</p>
-          </div>
-          <p className={styles.textBottom}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Praesent sagittis justo sed sapien blandit luctus. 
-          Phasellus lacinia tellus eget commodo facilisis. 
-          Cras facilisis est finibus justo consequat pellentesque. 
-          Integer rhoncus, nibh vel gravida dictum, orci tortor lacinia lacus, 
-          vitae scelerisque nisi odio at diam.
+        <div className={styles.div1}>
+          <Image src={imgJogo} className={styles.imgJogo}></Image>
+          <p className={styles.titleJogo}>Jogo</p>
+          <p className={styles.texto}>
+          Este projeto é um jogo de plataforma 
+          desenvolvido utilizando HTML5, CSS e JavaScript. 
+          O objetivo do jogo é controlar um personagem 
+          que deve desviar de obstáculos que surgem 
+          aleatoriamente, enquanto a dificuldade aumenta 
+          gradualmente com o tempo.
           </p>
-          <br></br>
-          <button className={styles.trocaDePagina} onClick={() => router.push('/page2')}>Todos os trabalhos</button>
-          <h1 className={styles.fontMarge}>Minhas ferramentas:</h1>
-          <br></br>
-          <Autoplay></Autoplay>
-          <br></br>
-          <h2 className={styles.conecte}>Conecte-se comigo</h2>
-          <br></br>
-          <br></br>
-          <footer className={styles.rodape}>
+        </div>
+        <div className={styles.div2}>
+        <Image src={imgGerenciador} className={styles.imgGerenciador}></Image>
+        <p className={styles.titleGerenciador}>Gerenciador de Livros</p> 
+          <p className={styles.texto2}>
+          Este projeto é um sistema de gerenciamento de livros 
+          desenvolvido utilizando HTML5, CSS e JavaScript. 
+          O objetivo principal é permitir que os usuários cadastrem, 
+          editem e excluam livros de uma lista, 
+          facilitando a organização de suas bibliotecas pessoais.
+          </p>
+        </div>
+        <div className={styles.div3}>
+          <Image src={imgPortfolio} className={styles.imgPortfolio}></Image>
+          <p className={styles.titlePortfolio}>Portfólio</p>
+          <p className={styles.texto3}>
+          Esta página serve como a introdução ao meu portfólio, 
+          apresentando uma interface moderna e interativa que reflete 
+          minhas habilidades em desenvolvimento web e design. 
+          A página foi desenvolvida utilizando Next.js, CSS Modules e JavaScript, 
+          com integração de animações e efeitos visuais para criar uma experiência envolvente.
+          </p>
+        </div>
+        <h2 className={styles.conecte}>Conecte-se comigo</h2>
+        <footer className={styles.rodape}>
             <a href="https://www.linkedin.com/in/vin%C3%ADcius-czechoski-coelho-340614310/" target="_blank" rel="noopener noreferrer" className={styles.link}>
               <div className={styles.divLinkedin}>
               <p>LinkedIn</p>
